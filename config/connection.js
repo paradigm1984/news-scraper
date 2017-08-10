66a5355c737225304f0469ce616797b6356cfb94
paradigm1984 <danblanco1990@gmail.com>
2017-08-10 18:05:01 -0400

    set up the db for heroku

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,10 @@ mongoose.connect("mongodb://localhost/newsScraper", function(error) {
 	console.log("Database connected");
 }); */
 
-// Local Database Configuration with Mongoose
-mongoose.connect("mongodb://heroku_4vgvv30r:@ds159217.mlab.com:59217/heroku_4vgvv30r", function(error) {
+mongoose.connect("mongodb://dblanco:qwert1@ds159217.mlab.com:59217/heroku_4vgvv30r", function(error) {
 	if(error) {
 		throw error;
 		console.log("there was an error connecting with the DB");
 	} 
 	console.log("Database connected");
-});
+}); 
