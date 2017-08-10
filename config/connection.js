53ce2ec052c089a5ef11ca39f520b77313a57220
paradigm1984 <danblanco1990@gmail.com>
2017-08-10 15:28:03 -0400

    added procfile

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 var mongoose = require("mongoose");
 
 // Local Database Configuration with Mongoose
+/*
 mongoose.connect("mongodb://localhost/newsScraper", function(error) {
 	if(error) {
 		throw error;
 		console.log("there was an error connecting with the DB");
 	} 
 	console.log("Database connected");
-});
+}); */
 
+// Local Database Configuration with Mongoose
+mongoose.connect("mongodb://heroku_4vgvv30r:@ds159217.mlab.com:59217/heroku_4vgvv30r", function(error) {
+	if(error) {
+		throw error;
+		console.log("there was an error connecting with the DB");
+	} 
+	console.log("Database connected");
+});
