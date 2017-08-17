453adab91219df802d96a6bb90290f1a2f1fe7a1
paradigm1984 <danblanco1990@gmail.com>
2017-08-17 12:03:52 -0400

    changed the db from local to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 var mongoose = require("mongoose");
 
 // Local Database Configuration with Mongoose
-
+/*
 mongoose.connect("mongodb://localhost/newsScraper", function(error) {
 	if(error) {
 		throw error;
 		console.log("there was an error connecting with the DB");
 	} 
 	console.log("Database connected");
-}); 
-/*
+}); */
+
 mongoose.connect("mongodb://dblanco:qwert1@ds159217.mlab.com:59217/heroku_4vgvv30r", function(error) {
 	if(error) {
 		throw error;
@@ -18,4 +18,3 @@ mongoose.connect("mongodb://dblanco:qwert1@ds159217.mlab.com:59217/heroku_4vgvv3
 	console.log("Database connected");
 }); 
 
-*/
